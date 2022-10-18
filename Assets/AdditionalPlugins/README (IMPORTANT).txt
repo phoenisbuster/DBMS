fcf0a9ee263ea90e30956f152f9a96d8ca29e9c0
phoenisbuster <phuc.nguyenphoenis@hcmut.edu.vn>
2022-10-18 21:06:38 +0700

    Add additional plugins (not currently used))
    Set project to Android
    Fix canvas scale

---
+++ Assets/AdditionalPlugins/README (IMPORTANT).txt
@@ -0,0 +1,3 @@
+Important: Do not unzip
+These files is currently unnessary, unzipping can make the project error!!!
+These contain additional dll files for future usage (maybe).
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
 using UnityEngine;
+using System.Threading;
 
 public class Test : MonoBehaviour
 {
+    public string dbUrl = "abc";
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        CreateDB();
+    }
+
+    public void CreateDB()
+    {
+        using(var connect = new SqliteConnection(dbUrl))
+        {
+            connect.OpenAsync(CancellationToken.None);
+
+            
+        }
     }
 
     // Update is called once per frame
